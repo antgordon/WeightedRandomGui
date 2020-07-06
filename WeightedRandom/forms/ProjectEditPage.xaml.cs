bf28589df48ea2ba404367c52581c69522093a4c
antgordon <56523753+antgordon@users.noreply.github.com>
2020-07-05 21:51:31 -0400

    Allow for editable entries in the table view

--- WeightedRandom/forms/ProjectEditPage.xaml.cs
+++ WeightedRandom/forms/ProjectEditPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections;
 using System.Collections.Generic;
 using System.Text;
@@ -8,6 +9,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -71,17 +73,29 @@ namespace WeightedRandom.forms
         public void SetTable(string name) {
             selected = project.GetTable(name);
 
+            SetTableRaw(selected);
+        }
+
+
+        protected void SetTableRaw(core.Table table)
+        {
+        
+
             if (selected == null)
             {
                 clearModel();
 
             }
-            else {
+            else
+            {
                 NormalTable tabe = selected.Normalize(project);
                 modelTree(project, selected, tabe);
                 displayTreeModel(selected, tabe, viewModel);
             }
         }
+
+
+
 
 
         private void modelTree(Project project, core.Table table, NormalTable normal)
@@ -130,7 +144,7 @@ namespace WeightedRandom.forms
          }
 
 
-        private TreeViewItem createTreeViewItem(string key, double raw, double normal) {
+        private TreeViewItem createTreeViewItem(string key, double raw, double normal, TreeNode node) {
             TreeViewItem treeViewItem = new TreeViewItem();
             StackPanel stackPanel = new StackPanel();
             stackPanel.Orientation = Orientation.Horizontal;
@@ -145,7 +159,30 @@ namespace WeightedRandom.forms
             TextBox rawText = new TextBox();
             rawText.Text = Convert.ToString(raw);
             rawText.Margin = new Thickness(0, 5, 10, 0);
+            core.Table selTbale = selected;
+            rawText.AcceptsReturn = false;
+            rawText.AcceptsTab = false;
+            rawText.KeyDown += (sender, e) =>
+            {
 
+                if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Escape)
+                {
+
+                    Keyboard.ClearFocus();
+                }
+            };
+
+            rawText.LostKeyboardFocus += (sender, e) =>
+            {
+
+
+                double value;
+                
+                if (double.TryParse(rawText.Text, out value)) { 
+                    node.TableRef.AddKey(node.TableKey.ShortName, value);
+                    SetTableRaw(selTbale);
+                }
+            };
             Label normalText = new Label();
             normalText.Content = $"{normal:P4}";
             normalText.Margin = new Thickness(20, 5, 10, 0);
@@ -176,7 +213,7 @@ namespace WeightedRandom.forms
 
         private TreeViewItem displayTreeModelRecursive(Project project, NormalTable normal, TreeNode node)
         {
-            TreeViewItem item = createTreeViewItem(node.Name, node.GetRawWeight(), node.GetNormalWeight(normal));
+            TreeViewItem item = createTreeViewItem(node.Name, node.GetRawWeight(), node.GetNormalWeight(normal), node);
 
             foreach (TreeNode child in node) {
                 TreeViewItem viewChild = displayTreeModelRecursive(project, normal, child);
@@ -187,16 +224,7 @@ namespace WeightedRandom.forms
         }
 
 
-        public void displayTree() {
-            core.Table table = project.GetTable("one");
-            NormalTable map = table.Normalize(project);
-            foreach ((core.Key key, double val) in map) {
-                double shortVal = table.GetWeight(key.ShortName);
-                TreeViewItem item = createTreeViewItem(key.ShortName, shortVal, val);
-                projectTree.Items.Add(item);
-            }
-
-        }
+    
 
 
         private void clearModel() {
