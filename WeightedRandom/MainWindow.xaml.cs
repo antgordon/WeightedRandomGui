5ca411f143f5f4248048c364b518bf4eb339ad9e
unknown <gordonad@umich.edu>
2021-01-07 13:42:08 -0500

    Complete basic render of table view menu

--- WeightedRandom/MainWindow.xaml.cs
+++ WeightedRandom/MainWindow.xaml.cs
@@ -13,6 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using WeightedRandom.core;
+using WeightedRandom.core2;
 using WeightedRandom.forms;
 
 namespace WeightedRandom
@@ -25,10 +26,16 @@ namespace WeightedRandom
         public MainWindow()
         {
             InitializeComponent();
-            Project project = new Project();
+            startCore2Page();
+        }
+
+
+
+        private void startCore1Page() {
+            core.Project project = new core.Project();
 
             core.Table pj = new core.Table("one");
-     
+
             pj.AddKey("one", 1.0);
             pj.AddKey("two", 1.0);
             pj.AddKey("three", 1.0);
@@ -51,12 +58,48 @@ namespace WeightedRandom
 
             project.RegisterTable(pj);
             project.RegisterTable(pj2);
-            project.RegisterTable(pj3); 
+            project.RegisterTable(pj3);
             pj.AddReference("one", "test2");
             pj2.AddReference("orange", "test3");
             pj.AddReference("three", "test3");
 
             Page page = new ProjectEditPage(project);
+            mainFrame.Navigate(page);
+        }
+
+        private void startCore2Page()
+        {
+            core2.Project project = new core2.Project("test", new IdAllocator());
+            IdAllocator allocator = project.Allocator;
+
+            core2.Table pj = allocator.CreateTable("one");
+            project.AddTable(pj);
+            pj.AddElement(allocator.CreateElement(pj,"one", 1.0));
+            pj.AddElement(allocator.CreateElement(pj, "two", 1.0));
+            pj.AddElement(allocator.CreateElement(pj, "three", 1.0));
+            pj.AddElement(allocator.CreateElement(pj, "four", 1.0));
+
+            core2.Table pj2 = allocator.CreateTable("test2");
+            project.AddTable(pj2);
+            pj2.AddElement(allocator.CreateElement(pj2,"apple", 1.0));
+            pj2.AddElement(allocator.CreateElement(pj2, "orange", 1.0));
+            pj2.AddElement(allocator.CreateElement(pj2, "banana", 1.0));
+            pj2.AddElement(allocator.CreateElement(pj2, "peach", 1.0));
+
+
+            core2.Table pj3 = allocator.CreateTable("test3");
+            project.AddTable(pj3);
+            pj3.AddElement(allocator.CreateElement(pj3, "Toyota", 0.5));
+            pj3.AddElement(allocator.CreateElement(pj3, "Ford", 5.0));
+            pj3.AddElement(allocator.CreateElement(pj3, "General Motors", 1.0));
+            pj3.AddElement(allocator.CreateElement(pj3, "Honda", 10.0));
+
+
+           // pj.AddReference("one", "test2");
+           // pj2.AddReference("orange", "test3");
+           // pj.AddReference("three", "test3");
+
+            Page page = new EditPage2(project);
             mainFrame.Navigate(page);
         }
     }
--- WeightedRandom/forms/EditPage2.xaml.cs
+++ WeightedRandom/forms/EditPage2.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 using System.Windows;
 using System.Windows.Controls;
@@ -21,33 +22,112 @@ namespace WeightedRandom.forms
     {
 
         private core2.Project project;
+        private ViewModel viewModel;
 
         public EditPage2(core2.Project project)
         {
+            InitializeComponent();
             this.project = project;
+            //SetTable("one");
+            loadSideButtons();
         }
 
+        private void loadSideButtons()
+        {
+            StackPanel stackPannel = new StackPanel();
+            Grid.SetColumn(stackPannel, 0);
+            Grid.SetRow(stackPannel, 1);
+
+            Grid grid = (Grid)this.Content;
+
+            foreach (core2.Table table in project)
+            {
+
+
+                Button but = creatButtonForTable(table);
+
+                stackPannel.Children.Add(but);
+
+            }
+
+            grid.Children.Add(stackPannel);
+
+
+
+        }
+
+        private Button creatButtonForTable(core2.Table table)
+        {
+            Button button = new Button();
+            button.Name = table.Name;
+            button.Content = table.Name;
+            button.Click += (obj, e) => { SetTable(button.Name); };
+            return button;
+        }
+
+        public void SetTable(string name)
+        {
+            core2.Table table = project.GetTable(name);
+
+            setTableRaw(table);
+        }
+
+
+        protected void setTableRaw(core2.Table table)
+        {
+            projectTree.Items.Clear();
+            if (viewModel != null)
+                viewModel.Clear();
+
+            if (table != null)
+            {
+                viewModel = new ViewModel(project, table, 1.0, projectTree.Items);
+                viewModel.Dispay();
+            }
+        }
+
+    
 
 
         class ViewModel {
 
             private core2.Table table;
+            private core2.Project project;
             public IDictionary<int, double> percentages;
             private IList<ViewModelTreeNode> nodes;
+            private ItemCollection displayNodes;
+            private double _basePercent;
 
 
-            public ViewModel(core2.Table table) {
+            public ViewModel(core2.Project project, core2.Table table, double basePercent, ItemCollection display) {
                 this.table = table;
-                percentages = table.ComputePercentage(1.0);
+                this.project = project;
+                this._basePercent = basePercent;
                 nodes = new List<ViewModelTreeNode>();
+                this.displayNodes = display;
+             
 
             }
 
+
+            public void Dispay() {
+                UpdatePercentage();
+
+                foreach (Element ele in table)
+                {
+                    ViewModelTreeNode node = new ViewModelTreeNode(this, ele);
+                    displayNodes.Add(node.GuiItem);
+                    nodes.Add(node);
+                }
+
+                UpdateAll();
+            }
+
             public void AddElement(string name, double weight) {
-                int newId = 0; //TODO assign some other way
-                Element element = new Element(newId, table.ID, name, weight);
+                Element element = project.Allocator.CreateElement(table, name, weight);
                 table.AddElement(element);
-                ViewModelTreeNode node = createTreeItem(nodes.Count);
+                ViewModelTreeNode node = new ViewModelTreeNode(this, element);
+                displayNodes.Add(node.GuiItem);
                 nodes.Add(node);
                 UpdatePercentage();
                 UpdateAll();
@@ -55,27 +135,26 @@ namespace WeightedRandom.forms
             }
 
 
-            public void RemoveElement(ViewModelTreeNode element)
+            public void RemoveElement(ViewModelTreeNode node)
             {
-          
-                if (nodes.Count > 0) {
-                    //taable remove inddex
-                    nodes.RemoveAt(nodes.Count - 1);
-                    UpdateAll();
-                }
+
+                table.RemoveElement(node.Element.ID);
+                nodes.Remove(node);
+                displayNodes.Remove(node.GuiItem);
+                UpdateAll();
                
             }
 
 
             public void UpdateAll() {
                 foreach (ViewModelTreeNode tree in nodes) {
-                    tree.updateSelf();
+                    tree.UpdateSelf();
                 }
             }
 
 
             public void UpdatePercentage() {
-                percentages = table.ComputePercentage(1.0);
+                percentages = table.ComputePercentage(_basePercent);
             }
 
             public void Reorder(int index, int secondIndex) {
@@ -87,10 +166,26 @@ namespace WeightedRandom.forms
             }
 
 
-            public ViewModelTreeNode createTreeItem(int index) {
+            public void Clear() {
+                foreach (ViewModelTreeNode tree in nodes)
+                {
+                    tree.Clear();
+                }
+            }
 
-                ViewModelTreeNode tree = new ViewModelTreeNode(this, index);
-                return tree;
+            private bool findTreeNode(int id, out ViewModelTreeNode node) {
+                node = null;
+
+                foreach (ViewModelTreeNode nodeSel in nodes) {
+
+                    if (nodeSel.Element.ID == id) {
+                        node = nodeSel;
+                        return true;
+                    }
+                }
+
+
+                return false;
             }
 
         }
@@ -99,50 +194,135 @@ namespace WeightedRandom.forms
         class ViewModelTreeNode
         {
 
-            private int index;
+            public Element Element { get; set; }
+            public TreeViewItem GuiItem { get; }
+           
             private ViewModel viewmodel;
-
             private Button closeButton;
             private TextBox nameBox;
             private TextBox weightBox;
             private Label percentageLabel;
-            StackPanel stackPanel;
+            private StackPanel stackPanel;
 
-            public ViewModelTreeNode(ViewModel viewmodel, int index)
+            public ViewModelTreeNode(ViewModel viewmodel, Element element)
             {
                 this.viewmodel = viewmodel;
+                this.Element = element;
+                
+                closeButton = new Button();
+                closeButton.Content = "XX";
+                closeButton.Margin = new Thickness(0, 5, 10, 0);
+                closeButton.Click += CloseButton_Click;
+
+                nameBox = new TextBox();
+                nameBox.AcceptsReturn = false;
+                nameBox.AcceptsTab = false;
+                nameBox.KeyDown += Unfocus_KeyDown;
+                nameBox.LostKeyboardFocus += NameBox_LostKeyboardFocus;
+                nameBox.Margin = new Thickness(0, 5, 10, 0);
+
+                weightBox = new TextBox();
+                weightBox.AcceptsReturn = false;
+                weightBox.AcceptsTab = false;
+                weightBox.KeyDown += Unfocus_KeyDown;
+                weightBox.LostKeyboardFocus += WeightBox_LostKeyboardFocus;
+                weightBox.Margin = new Thickness(0, 5, 10, 0);
+
+                percentageLabel = new Label();
+                percentageLabel.Margin = new Thickness(20, 5, 10, 0);
+
                 stackPanel = new StackPanel();
                 stackPanel.Orientation = Orientation.Horizontal;
-                closeButton = new Button();
-                nameBox = new TextBox();
-                weightBox = new TextBox();
-                percentageLabel = new Label();
-                this.index = index;
+                stackPanel.Children.Add(closeButton);
+                stackPanel.Children.Add(nameBox);
+                stackPanel.Children.Add(weightBox);
+                stackPanel.Children.Add(percentageLabel);
 
-
-                closeButton.Click += CloseButton_Click;
+                GuiItem = new TreeViewItem();
+                GuiItem.Header = stackPanel;
 
             }
 
+
+            /// <summary>
+            /// Parse namebox and set name
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void NameBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+            {
+                Element.Name = nameBox.Text;
+                UpdateSelf();
+            }
+
+            /// <summary>
+            /// Parse weight and set weight
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
+            private void WeightBox_LostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
+            {
+                double value;
+
+                if (double.TryParse(weightBox.Text, out value))
+                {
+                    Element.Weight = value;
+                    Reweight();
+
+
+                }
+            }
+
+
+            //
+            //
+            //Unfocus a textbox when enter or excape is pressed
+            //
+            private void Unfocus_KeyDown(object sender, KeyEventArgs e)
+            {
+                if (e.Key == System.Windows.Input.Key.Enter || e.Key == System.Windows.Input.Key.Escape)
+                {
+
+                    Keyboard.ClearFocus();
+                }
+            }
+
+
+            /// <summary>
+            /// Close the button removes the tree
+            /// </summary>
+            /// <param name="sender"></param>
+            /// <param name="e"></param>
             private void CloseButton_Click(object sender, RoutedEventArgs e)
             {
                 viewmodel.RemoveElement(this);
             }
 
-            public void updateSelf() {
-                Element element = null;
-                nameBox.Text = element.Name;
-                weightBox.Text = $"{element.Weight:F2}";
-                double percentage = viewmodel.percentages[element.ID];
-                percentageLabel.Content = $"{percentage:F2}";
+
+
+            public void UpdateSelf() {
+               
+                nameBox.Text = Element.Name;
+                weightBox.Text = $"{Element.Weight:F2}";
+                double percentage = viewmodel.percentages[Element.ID];
+                percentageLabel.Content = $"{percentage:F4}";
 
             }
 
-            public void reweight() {
+            public void Reweight() {
                 //Parse weight
                 //Apply to element in table
                 viewmodel.UpdatePercentage();
                 viewmodel.UpdateAll();
+            }
+
+
+            public void Clear() {
+                stackPanel.Children.Clear();
+                GuiItem.Header = null;
+                viewmodel = null;
+                Element = null;
+
             }
         }
     }
